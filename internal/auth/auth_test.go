9f6cee7f89c97bae0c44acb1bb2d45e0f319f62a
Manhnguyen981024 <manhtititaka@gmail.com>
2026-03-25 14:43:57 +0000

    update fail ci

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetApiKey(t *testing.T) {
+	header := http.Header{}
+	if _, err := GetAPIKey(header); err == nil {
+		t.Fatalf("Expected: ErrNoAuthHeaderIncluded but got %v", err)
+	}
+	header.Add("Authorization", "ABC123")
+
+	if _, err := GetAPIKey(header); err == nil {
+		t.Fatalf("Expected: Emalformed authorizatio but got %v", err)
+	}
+
+	expectedApiKey := "abc123"
+	header.Set("Authorization", "ApiKey 1"+expectedApiKey)
+	actualApiKey, err := GetAPIKey(header)
+	if err != nil {
+		t.Fatalf("Expected: get API key success with key abc123 but got an error %v", err)
+	}
+
+	if expectedApiKey != actualApiKey {
+		t.Fatalf("Expected: get API key with value %v but got an %v", expectedApiKey, actualApiKey)
+	}
+	t.Log("Test success")
+}
